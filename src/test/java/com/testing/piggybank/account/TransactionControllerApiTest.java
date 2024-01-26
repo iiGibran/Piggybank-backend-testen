f9cbc569d487c8d5ddb01608f8dbb4d19112b6e1
iiGibran <90310030+iiGibran@users.noreply.github.com>
2024-01-26 15:30:42 +0100

    Update TransactionControllerApiTest.java

--- src/test/java/com/testing/piggybank/account/TransactionControllerApiTest.java
+++ src/test/java/com/testing/piggybank/account/TransactionControllerApiTest.java
@@ -1,5 +1,6 @@
 package com.testing.piggybank.account;
 
+import com.testing.piggybank.model.Currency;
 import com.testing.piggybank.transaction.CreateTransactionRequest;
 import com.testing.piggybank.transaction.GetTransactionsResponse;
 import org.junit.jupiter.api.Test;
@@ -7,24 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TransactionControllerApiTest {
 
-    // Injecting local server port
     @LocalServerPort
     private int port;
 
-    // Autowiring the TestRestTemplate to interact with the API
     @Autowired
     private TestRestTemplate restTemplate;
 
-    // Test method for GET endpoint to retrieve transactions
+
     @Test
     public void testGetTransactions() {
-        // Setting up test data
         long accountId = 123;
         String url = "http://localhost:" + port + "/api/v1/transactions/" + accountId;
 
@@ -33,28 +35,32 @@ public class TransactionControllerApiTest {
                 restTemplate.getForEntity(url, GetTransactionsResponse.class);
 
         // Asserting that the HTTP status code is 200 (OK)
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(HttpStatus.OK.value());
 
         GetTransactionsResponse response = responseEntity.getBody();
         assertThat(response).isNotNull();
 
-        // Asserting that the list of transactions in the response is empty
+        // Asserting that the list of transactions in the response is empty for this account
         assertThat(response.getTransactions()).isEmpty();
     }
 
-    // Test method for POST endpoint to create a transaction
     @Test
     public void testCreateTransaction() {
         // Setting up the URL for the POST request
         String url = "http://localhost:" + port + "/api/v1/transactions";
 
-        // Creating an empty transaction request
+        // Creating a valid transaction request
         CreateTransactionRequest request = new CreateTransactionRequest();
+        request.setSenderAccountId(1L); // Provide valid account IDs
+        request.setReceiverAccountId(2L);
+        request.setAmount(BigDecimal.valueOf(100.0)); // Provide a valid amount
+        request.setCurrency(Currency.valueOf("USD"));
+        request.setDescription("Test Transaction");
 
         // Sending a POST request to create a transaction
         ResponseEntity<Void> responseEntity = restTemplate.postForEntity(url, request, Void.class);
 
-        // Asserting that the HTTP status code is 500
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(500);
+        // Asserting that the HTTP status code is 200 (OK) or any other expected status for a successful transaction
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(HttpStatus.OK.value());
     }
 }
